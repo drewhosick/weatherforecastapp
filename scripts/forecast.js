428a88aee5f89c36a4dc5e6f49fbda1745c2bd66
drewhosick <drewhosick@gmail.com>
2021-09-29 21:10:27 -0400

    completed project

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,6 @@
 class Forecast {
     constructor(){
-      this.key = 'xvpOBAAypFh84YftzPvUCh8ZM80gbYIG';
+      this.key = 'VRMoAKnp8oMF80fIjwLfAwg3MYoLDBRW';
       this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
       this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     }
