5ff9c3c0298dd5d7e5c64ee9559e3f4cecc42745
Drew Hosick <drewhosick@gmail.com>
2021-10-11 22:01:27 -0400

    Updated to HTTPS for API calls

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast {
     constructor(){
       this.key = 'VRMoAKnp8oMF80fIjwLfAwg3MYoLDBRW';
-      this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
-      this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+      this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
+      this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     }
     async updateCity(city){
       const cityDets = await this.getCity(city);
